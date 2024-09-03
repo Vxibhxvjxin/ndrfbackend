34b7ac66fd804be85e321e825db100ab32e58a4a
Vxibhxvjxin <vaibhavjainsept052004@gmail.com>
2024-09-03 18:34:57 +0530

    Updated Home-landing.js

--- my-app/src/Components/Landingpage/Home-landing.js
+++ my-app/src/Components/Landingpage/Home-landing.js
@@ -1,54 +1,19 @@
-// Home.js
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Navbar from "./Home-navbar";
-import Popup from "../Common/Popup";
-import BannerBackground from "../../Assets/home-banner-background.png";
-import BannerImage from "../../Assets/home-banner-image.png";
-import { FiArrowRight } from "react-icons/fi";
+import Navbar from "../Aboutpage/Abt-navbar.js";
+import Popup from "../Common/Popup.js";
 
-const Home = ({ triggerPopup, isPopupVisible, closePopup }) => {
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate('/login');
-  };
-
+const HomePage = ({ triggerPopup, isPopupVisible, closePopup }) => {
   useEffect(() => {
     triggerPopup(); // Trigger popup only once on component mount
   }, [triggerPopup]);
 
   return (
-    <div className="home-container" id="home">
+    <div className="home-page">
       <Navbar triggerPopup={triggerPopup} />
       {isPopupVisible && <Popup />} {/* Conditionally render Popup */}
-      <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="Banner Background" />
-        </div>
-
-        <div>
-          <h1 className="primary-heading">
-            Real-Time Disaster Information Aggregation Software
-          </h1>
-          <p className="primary-text">
-            <span className="tab"></span>Uddhrti harnesses real-time data and AI
-            to revolutionize disaster management, delivering swift, accurate
-            insights and empowering rescue teams to act decisively. Our platform
-            ensures efficient coordination, enhancing the ability to save lives
-            and mitigate damage.
-          </p>
-          <button className="secondary-button" onClick={handleButtonClick}>
-            AI Dashboard <FiArrowRight />{" "}
-          </button>
-        </div>
-
-        <div className="home-image-section">
-          <img src={BannerImage} alt="Banner Image" />
-        </div>
-      </div>
+      {/* Home page content */}
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
